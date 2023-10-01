c48549237a19b8b3e851a2c70a46c6cb85102713
Waliullah Ferdous <waliullah.ferdous22@gmail.com>
2023-10-02 03:24:36 +1100

    fixed port

--- server/server.js
+++ server/server.js
@@ -5,7 +5,7 @@ const path = require('path')
 require('dotenv').config()
 
 const app = express()
-const port = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 
 app.use(cors())
 app.use(express.static(path.join(__dirname, '..', 'static')))
