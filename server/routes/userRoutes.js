66d67628a329948bed5a351f0434dc8cf2e32a90
Avash N eupane <126855677+Avash12345@users.noreply.github.com>
2023-10-10 22:52:06 +0530

    Update userRoutes.js added the get user route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,10 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userController = require('../controllers/userController');
+const userController = require("../controllers/userController");
+const jwtUtils = require("../utils/jwtUtils");
 
-
-router.post('/signup', userController.signUp);
-router.post('/login', userController.logIn);
+router.post("/signup", userController.signUp);
+router.post("/login", userController.logIn);
+router.get("/user", jwtUtils.verifyToken, userController.getUser);
 
 module.exports = router;
