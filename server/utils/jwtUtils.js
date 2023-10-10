d81b3977882fa421e9dba35d67be54d8d67a0366
Avash N eupane <126855677+Avash12345@users.noreply.github.com>
2023-10-10 22:52:41 +0530

    Update jwtUtils.js minor changes to very token

--- server/utils/jwtUtils.js
+++ server/utils/jwtUtils.js
@@ -1,13 +1,13 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const secret = process.env.JWT_SECRET;
-
+const User = require("../models/userSchema");
 exports.createToken = (userId) => {
 	const token = jwt.sign({ id: userId }, secret, { expiresIn: "7h" });
 	return token;
 };
 
-exports.verifyToken = (req, res, next) => {
+exports.verifyToken = async (req, res, next) => {
 	try {
 		let token = req.headers.authorization;
 		if (!token) {
@@ -21,8 +21,11 @@ exports.verifyToken = (req, res, next) => {
 		if (!verified) {
 			return res.status(401).json({ error: "Unauthorized" });
 		}
+
+		req.user = await User.findById(verified.id);
 		next();
 	} catch (err) {
+		console.error(err);
 		return res.status(500).json({ error: "Internal Server Error" });
 	}
 };
