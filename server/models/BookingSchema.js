78f95a5d48f6e98899612ec5bfa84654bf631a45
Avash N eupane <126855677+Avash12345@users.noreply.github.com>
2023-10-15 21:35:06 +0530

    created the booking schema

---
+++ server/models/BookingSchema.js
@@ -0,0 +1,27 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+require("dotenv").config();
+
+url = process.env.MONGODB_B_URI;
+
+mongoose
+	.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => {
+		console.log("Connected to MongoDB");
+	})
+	.catch((error) => {
+		console.log("Error connecting to MongoDB:", error.message);
+	});
+
+const bookingSchema = new Schema({
+	userId: { type: Schema.Types.ObjectId, ref: "User" },
+	serviceId: { type: Schema.Types.ObjectId, ref: "Service" },
+	serviceProviderId: { type: Schema.Types.ObjectId, ref: "ServiceProvider" },
+	date: { type: Date, required: true },
+	bookingLocation: { type: String, required: true },
+	bookingStatus: { type: String, required: true },
+}); //hashing password
+
+const Booking = mongoose.model("Booking", bookingSchema);
+
+module.exports = Booking;
