bf1af5f0af23a6ac68a7e800469efdc08e66326c
wonke1 <100748961+wonke1@users.noreply.github.com>
2023-10-15 20:55:04 +1100

    added SP token verification and route

--- server/routes/serviceProviderRoutes.js
+++ server/routes/serviceProviderRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const serviceProviderController = require('../controllers/serviceProviderController')
+const jwtUtils = require('../utils/jwtUtils')
 
 router.post('/SPsignup', serviceProviderController.signUp)
 router.post('/SPlogin', serviceProviderController.logIn)
+router.get('/SPuser', jwtUtils.SPverifyToken, serviceProviderController.getUser)
 
 module.exports = router
--- server/utils/jwtUtils.js
+++ server/utils/jwtUtils.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 require("dotenv").config();
 const secret = process.env.JWT_SECRET;
 const User = require("../models/userSchema");
+const ServiceProvider = require("../models/serviceProviderSchema")
 exports.createToken = (userId) => {
 	const token = jwt.sign({ id: userId }, secret, { expiresIn: "7h" });
 	return token;
@@ -25,7 +26,28 @@ exports.verifyToken = async (req, res, next) => {
 		req.user = await User.findById(verified.id);
 		next();
 	} catch (err) {
+		console.error(err);                         
+		return res.status(500).json({ error: "Internal Server Error" });
+	}
+}
+
+exports.SPverifyToken = async (req, res, next) => {
+	try {
+		let token = req.headers.authorization;
+		if (!token) {
+			return res.status(401).json({ error: "Unauthorized" });
+		}
+		if (token.startsWith("Bearer")) {
+			token = token.slice(7, token.length).trimLeft();
+		}
+		const verified = jwt.verify(token, secret);
+		if (!verified) {
+			return res.status(401).json({ error: "Unauthorized" });
+		}
+		req.user = await ServiceProvider.findById(verified.id);
+		next();
+	} catch (err) {
 		console.error(err);
 		return res.status(500).json({ error: "Internal Server Error" });
 	}
-};
+}
