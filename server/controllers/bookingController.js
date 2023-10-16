49fede9dc6ed17d8b484d81b98cc8b56be07ebe8
Avash N eupane <126855677+Avash12345@users.noreply.github.com>
2023-10-16 22:55:35 +0530

    minor changes

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -1,5 +1,7 @@
 const Booking = require("../models/BookingSchema");
-
+const User = require("../models/UserSchema");
+const Service = require("../models/ServiceSchema");
+const ServiceProvider = require("../models/ServiceProviderSchema");
 
 exports.createBooking = async (req, res) => {
 	try {
@@ -26,14 +28,16 @@ exports.createBooking = async (req, res) => {
 };
 
 exports.getBookingByUserId = async (req, res) => {
-    try {
-        const { userId } = req.params;
-        const booking = await Booking.find({ userId })
-        if(bookings.length === 0) {
-            return res.status(404).json({ status: "fail", message: "No bookings found" });
-        }    
-        res.status(200).json({ status: "success", booking });
-    } catch (error) {
-        res.status(500).json({ status: "fail", error: error.message });
-    }
+	try {
+		const { userId } = req.params;
+		const booking = await Booking.find({ userId });
+		if (booking.length === 0) {
+			return res
+				.status(404)
+				.json({ status: "fail", message: "No bookings found" });
+		}
+		res.status(200).json({ status: "success", booking });
+	} catch (error) {
+		res.status(500).json({ status: "fail", error: error.message });
+	}
 };
