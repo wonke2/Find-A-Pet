e956d514459973269724625e53a08b07e4ced5cd
wonke1 <100748961+wonke1@users.noreply.github.com>
2023-10-16 13:30:14 +1100

    fixed service listings

--- src/pages/ServiceListings.js
+++ src/pages/ServiceListings.js
@@ -1,26 +1,23 @@
+// ServiceListings.js
 import React, { useState, useEffect } from 'react';
 
 const ServiceListings = () => {
-    const [providers, setProviders] = useState([]);
+    const [services, setServices] = useState([]);
 
     useEffect(() => {
-        // Fetching the services from the provided API endpoint
         fetch('/api/services/')
             .then(response => response.json())
-            .then(data => setProviders(data))
+            .then(data => setServices(data))
             .catch(error => console.error('Error fetching services:', error));
-    }, []); // The empty array means this effect will run once when the component mounts
+    }, []);
 
     return (
         <div className="service-listings">
-            {providers.map((provider, index) => (
-                <div key={index} className="provider">
-                    <h3>{provider.serviceProviderName}</h3>
-                    {provider.servicesProvided.map((service, sIndex) => (
-                        <div key={sIndex} className="service">
-                            {service}
-                        </div>
-                    ))}
+            {services.map((service) => (
+                <div key={service._id} className="service">
+                    <h3>{service.serviceName}</h3>
+                    <p>{service.serviceDescription}</p>
+                    <small>Location: {service.serviceLocation}</small>
                 </div>
             ))}
         </div>
