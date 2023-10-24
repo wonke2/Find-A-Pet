88b3954dccd774ae0843d6182fb0f0b4967a1394
Avash N eupane <126855677+Avash12345@users.noreply.github.com>
2023-10-24 08:13:52 +0530

    configured global state

---
+++ src/state/index.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+	token: null,
+};
+
+const authSlice = createSlice({
+	name: "auth",
+	initialState,
+	reducers: {
+		setLogin: (state, action) => {
+			state.token = action.payload.token;
+		},
+		setLogout: (state) => {
+			state.token = null;
+		},
+	},
+});
+
+export const { setLogin, setLogout } = authSlice.actions;
+export default authSlice.reducer;
