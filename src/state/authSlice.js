1af4578dfe1cf33cc83d9082a747d6d6ae2c0463
Avash N eupane <126855677+Avash12345@users.noreply.github.com>
2023-10-24 14:21:19 +1100

    updated the state

---
+++ src/state/authSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+	token: null,
+};
+
+export const authSlice = createSlice({
+	name: "auth",
+	initialState,
+	reducers: {
+		setLogin: (state, action) => {
+			state.token = action.payload.token;
+		},
+		setLogout: (state) => {
+			state.token = null;
+		},
+	},
+});
+
+
+export const {setLogin,setLogout} = authSlice.actions;
+
+
+export default authSlice.reducer;
