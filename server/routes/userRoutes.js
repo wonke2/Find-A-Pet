0a2c8725f40d7a6a4f7a4338b3408b9b30e87080
Avash N eupane <126855677+Avash12345@users.noreply.github.com>
2023-10-24 08:54:05 +0530

    added the booking route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/userController");
+const bookingController = require("../controllers/bookingController");
 const jwtUtils = require("../utils/jwtUtils");
 
 router.post("/signup", userController.signUp);
 router.post("/login", userController.logIn);
 router.get("/user", jwtUtils.verifyToken, userController.getUser);
-
+router.get(
+	"/booking",
+	jwtUtils.verifyToken,
+	bookingController.getBookingByUserId
+);
+router.post(
+	"/booking/create",
+	jwtUtils.verifyToken,
+	bookingController.createBooking
+);
 module.exports = router;
