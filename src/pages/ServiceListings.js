581346f6d5b1c7e83db49d588bd96e620650b5df
wonke1 <100748961+wonke1@users.noreply.github.com>
2023-10-29 23:02:54 +1100

    added needed imports to files

--- src/pages/ServiceListings.js
+++ src/pages/ServiceListings.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import axios from 'axios';
--- src/pages/UserSignup.js
+++ src/pages/UserSignup.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link } from "react-router-dom";
 import "../styles/UserSignup.css";
 const UserSignup = () => {
 	const [username, setUsername] = useState("");
