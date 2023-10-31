85d54772d58eb7d4ff95fc58d4832e9be30f2e2d
Avash N eupane <126855677+Avash12345@users.noreply.github.com>
2023-11-01 00:32:19 +0530

    updated the signup page according to the requirements

--- src/pages/UserSignup.js
+++ src/pages/UserSignup.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import validator from "validator";
-import "../styles/UserSignup.css";
-import { set } from "mongoose";
+
 const UserSignup = () => {
 	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
@@ -10,21 +9,66 @@ const UserSignup = () => {
 	const [phoneNo, setPhoneNo] = useState("");
 	const [address, setAddress] = useState("");
 	const [err, setErr] = useState("");
+	const [pherr, setPherr] = useState(null);
+	const [emailerr, setEmailerr] = useState("");
+	const [usererr, setUsererr] = useState("");
 	const navigate = useNavigate();
 	const checkPassword = (event) => {
 		var pass = event.target.value;
 		const reg = new RegExp(
 			"^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])(?=.{8,})"
 		);
-		var test = reg.test(pass);
-		if (test) {
-			setErr("");
-			console.log(pass);
-			setPassword(pass);
+		if (pass) {
+			var test = reg.test(pass);
+			if (test) {
+				setPassword(pass);
+			} else {
+				setPassword("");
+				setErr(
+					"Password must contain 8 characters, 1 uppercase, 1 lowercase, 1 number and 1 special character"
+				);
+			}
 		} else {
-			setErr(
-				"Password must contain 8 characters, 1 uppercase, 1 lowercase"
-			);
+			setPassword("");
+			setErr("Password cannot be empty");
+		}
+	};
+	const checkPhone = (event) => {
+		var phoneNo = event.target.value;
+
+		if (phoneNo) {
+			if (phoneNo.length === 10) {
+				setPhoneNo(phoneNo);
+			} else {
+				setPhoneNo("");
+				setPherr("Phone number must be 10 digits");
+			}
+		} else {
+			setPhoneNo("");
+			setPherr("Phone number cannot be empty");
+		}
+	};
+	const checkUsername = (event) => {
+		const username = event.target.value;
+		if (username) {
+			setUsername(username);
+		} else {
+			setUsername("");
+			setUsererr("Username cannot be empty");
+		}
+	};
+	const checkEmail = (event) => {
+		var email = event.target.value;
+		if (email) {
+			var test = validator.isEmail(email);
+			if (test) {
+				setEmail(email);
+			} else {
+				setEmailerr("Invalid Email");
+			}
+		} else {
+			setEmail("");
+			setEmailerr("Email cannot be empty");
 		}
 	};
 	const signup = async () => {
@@ -32,16 +76,7 @@ const UserSignup = () => {
 			alert("Please fill all the required fields");
 			return;
 		}
-		const validPhoneNo = validator.isMobilePhone(phoneNo);
-		const validEmail = validator.isEmail(email);
-		if (!validPhoneNo) {
-			alert("Invalid Phone Number");
-			return;
-		}
-		if (!validEmail) {
-			alert("Invalid Email");
-			return;
-		}
+
 		const res = await fetch("http://localhost:3000/auth/signup", {
 			method: "POST",
 			headers: {
@@ -64,57 +99,67 @@ const UserSignup = () => {
 			navigate("/userlogin");
 		}
 	};
+	console.log(phoneNo);
 	return (
-		<div className="main_form_signup">
-			<h1>User Signup</h1>
-			<div className="form_inp">
+		<div className="center">
+			<form>
+				<h3>Register</h3>
+
+				<label>
+					Username <i>*</i>
+				</label>
 				<input
-					className="required"
 					type="text"
-					placeholder="Username"
-					onChange={(e) => {
-						setUsername(e.target.value);
-					}}
+					placeholder="username"
+					id="username"
+					onChange={checkUsername}
 				/>
-				<p>{err}</p>
+				<p className={`err ${username ? "hidden" : ""}`}>{usererr}</p>
+				<label>
+					Password <i>*</i>
+				</label>
 				<input
-					className="required"
 					type="password"
 					placeholder="Password"
+					id="password"
 					onChange={checkPassword}
 				/>
+				<p className={`err ${password ? "hidden" : ""}`}>{err}</p>
+				<label>Address</label>
 				<textarea
-					name=""
+					name="address"
 					id=""
-					rows="3"
+					cols="30"
+					rows="10"
 					placeholder="Address"
-					onChange={(e) => {
-						setAddress(e.target.value);
-					}}
-				/>
+					onChange={(event) => {
+						setAddress(event.target.value);
+					}}></textarea>
+				<label>
+					Email <i>*</i>
+				</label>
 				<input
-					className="required"
 					type="email"
-					placeholder="email"
-					onChange={(e) => {
-						setEmail(e.target.value);
-					}}
+					placeholder="Email"
+					id="email"
+					onChange={checkEmail}
 				/>
+				<p className={`err ${email ? "hidden" : ""}`}>{emailerr}</p>
+				<label>
+					Phone Number <i>*</i>
+				</label>
 				<input
-					className="required"
-					type="number"
-					placeholder="phoneNo"
-					onChange={(e) => {
-						setPhoneNo(e.target.value);
-					}}
+					type="text"
+					placeholder="Phone Number"
+					onChange={checkPhone}
 				/>
-				<button className="login_button" onClick={signup}>
-					Signup
-				</button>
-				<Link to="/spsignup" className="nav-element">
-					Service Provider Signup
-				</Link>
-			</div>
+				<p className={`err ${phoneNo ? "hidden" : ""}`}>{pherr}</p>
+				<div className="button" onClick={signup}>Signup</div>
+				<div className="social">
+					<Link to="/userlogin"> Login </Link>
+					<Link className="/spsignup"> Service Provider</Link>
+				</div>
+			</form>
 		</div>
 	);
 };
