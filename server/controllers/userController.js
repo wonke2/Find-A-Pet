0983389dd318ed70b955c426e5ba079ad2a27a04
Avash N eupane <126855677+Avash12345@users.noreply.github.com>
2023-11-01 00:31:09 +0530

    changest to the signup logic

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,6 +2,13 @@ const User = require("../models/userSchema");
 const jwtUtils = require("../utils/jwtUtils");
 exports.signUp = async (req, res) => {
 	const { username, password, email, phoneNo, address } = req.body;
+	if (!username || !password || !email || !phoneNo ) {
+		return res.status(400).json({
+			status: "fail",
+			message: "Please fill all the required fields",
+		});
+	}
+	address ? (address = address) : (address = "");
 	try {
 		const user = await User.create({
 			username,
@@ -10,22 +17,22 @@ exports.signUp = async (req, res) => {
 			phoneNo,
 			address,
 		});
-		if (user) {
-			res.status(200).json({
-				status: "success",
-				data: user,
-			});
-		} else {
-			res.status(400).json({
+		res.status(200).json({
+			status: "success",
+			data: user,
+		});
+	} catch (err) {
+		// Check for unique constraint violation
+		if (err.code === 11000) {
+			return res.status(500).json({
 				status: "fail",
-				message: "user not created",
+				message: "User already exists",
 			});
 		}
-	} catch (err) {
-		res.status(500).json({
+		// Handle other errors
+		res.status(400).json({
 			status: "fail",
-
-			message: "user already exists",
+			message: "User not created",
 		});
 	}
 };
@@ -66,60 +73,70 @@ exports.getUser = async (req, res) => {
 
 exports.addToWishlist = async (req, res) => {
 	try {
-        const { petID, petName, petImage } = req.body;
+		const { petID, petName, petImage } = req.body;
 
-        // Check if the pet is already in the wishlist
-        const user = await User.findById(req.user._id);
-        if (user.wishlist.some(pet => pet.petID === petID)) {
-            return res.status(400).json({ status: "fail", message: "Pet already in wishlist" });
-        }
+		// Check if the pet is already in the wishlist
+		const user = await User.findById(req.user._id);
+		if (user.wishlist.some((pet) => pet.petID === petID)) {
+			return res
+				.status(400)
+				.json({ status: "fail", message: "Pet already in wishlist" });
+		}
 
-        // If not, add the pet to the wishlist
-        user.wishlist.push({ petID, petName, petImage });
-        await user.save();
+		// If not, add the pet to the wishlist
+		user.wishlist.push({ petID, petName, petImage });
+		await user.save();
 
-        res.status(200).json({ status: "success", message: "Added to wishlist" });
-    } catch (error) {
-        res.status(500).json({ status: "fail", message: error.message });
-    }
+		res.status(200).json({
+			status: "success",
+			message: "Added to wishlist",
+		});
+	} catch (error) {
+		res.status(500).json({ status: "fail", message: error.message });
+	}
 };
 
 exports.getWishlist = async (req, res) => {
-    try {
-        const user = await User.findById(req.user._id);
-        if (!user) {
-            return res.status(404).json({
-                status: "fail",
-                message: "User not found",
-            });
-        }
-        return res.status(200).json({
-            status: "success",
-            wishlist: user.wishlist
-        });
-    } catch (error) {
-        return res.status(500).json({
-            status: "fail",
-            message: error.message,
-        });
-    }
+	try {
+		const user = await User.findById(req.user._id);
+		if (!user) {
+			return res.status(404).json({
+				status: "fail",
+				message: "User not found",
+			});
+		}
+		return res.status(200).json({
+			status: "success",
+			wishlist: user.wishlist,
+		});
+	} catch (error) {
+		return res.status(500).json({
+			status: "fail",
+			message: error.message,
+		});
+	}
 };
 
 exports.removeFromWishlist = async (req, res) => {
-    try {
-        const { petID } = req.body;
+	try {
+		const { petID } = req.body;
 
-        // Find the user
-        const user = await User.findById(req.user._id);
+		// Find the user
+		const user = await User.findById(req.user._id);
 
-        // Remove the pet from the wishlist
-        const updatedWishlist = user.wishlist.filter(pet => pet.petID !== petID);
-        user.wishlist = updatedWishlist;
+		// Remove the pet from the wishlist
+		const updatedWishlist = user.wishlist.filter(
+			(pet) => pet.petID !== petID
+		);
+		user.wishlist = updatedWishlist;
 
-        await user.save();
+		await user.save();
 
-        res.status(200).json({ status: "success", message: "Removed from wishlist" });
-    } catch (error) {
-        res.status(500).json({ status: "fail", message: error.message });
-    }
+		res.status(200).json({
+			status: "success",
+			message: "Removed from wishlist",
+		});
+	} catch (error) {
+		res.status(500).json({ status: "fail", message: error.message });
+	}
 };
