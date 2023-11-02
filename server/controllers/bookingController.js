e823be37d07203e38bfdf769e6d52988e73ee163
Waliullah Ferdous <waliullah.ferdous22@gmail.com>
2023-11-02 19:35:47 +1100

    adjusted new service type to create/fetch booking

--- server/controllers/bookingController.js
+++ server/controllers/bookingController.js
@@ -3,28 +3,40 @@ const Booking = require('../models/BookingSchema');
 // Create a booking
 exports.createBooking = async (req, res) => {
     try {
-        const { userID, serviceProviderID, serviceID } = req.body;
+        const { userID, serviceProviderID, serviceIndex } = req.body;
         
         const booking = await Booking.create({
-			userID, 
-			serviceProviderID, 
-			serviceID 
-		});
-		res.status(201).json({ status: "success", booking });
-	} catch (error) {
-		res.status(500).json({ status: "fail", error: error.message });
-	}
+            userID, 
+            serviceProviderID, 
+            serviceIndex
+        });
+        res.status(201).json({ status: "success", booking });
+    } catch (error) {
+        res.status(500).json({ status: "fail", error: error.message });
+    }
 };
+
 
 // Get bookings for user
 exports.getUserBookings = async (req, res) => {
     try {
         const { userID } = req.params;
 
-        const bookings = await Booking.find({ userID }).populate('serviceID', 'serviceName').populate('serviceProviderID', 'serviceProviderName');
+        const bookings = await Booking.find({ userID })
+            .populate('serviceProviderID', 'servicesProvided serviceProviderName');
 
-        res.status(200).json(bookings);
+        const populatedBookings = bookings.map(booking => {
+            const service = booking.serviceProviderID.servicesProvided[booking.serviceIndex];
+            return {
+                ...booking._doc,
+                service,
+                serviceProviderName: booking.serviceProviderID.serviceProviderName
+            };
+        });
+
+        res.status(200).json(populatedBookings);
     } catch (error) {
+        console.error("Error in getUserBookings:", error);
         res.status(500).json({ error: error.message });
     }
 };
