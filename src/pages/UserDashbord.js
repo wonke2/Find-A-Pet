3503f43ef556de06c1fdbdda3eb6d07e5592e5ad
Waliullah Ferdous <waliullah.ferdous22@gmail.com>
2023-11-02 19:37:33 +1100

    linked booking to userdashboard

--- src/pages/UserDashbord.js
+++ src/pages/UserDashbord.js
@@ -4,103 +4,26 @@ import { useState, useEffect } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-const UserDashbord = () => {
-	const navigate = useNavigate();
-	const [bookings, setBookings] = useState([]);
-	const token = useSelector((state) => state.token);
-	const [loading, setLoading] = useState(true);
-	useEffect(() => {
-		if (!token) {
-			navigate("/userlogin");
-		} else {
-			fetch("http://localhost:3000/auth/booking", {
-				headers: {
-					Authorization: `Bearer ${token}`,
-				},
-			})
-				.then((res) => res.json())
-				.then((data) => {
-					if (data.status === "fail") {
-						alert(data.message);
-						setLoading(false);
-					} else {
-						setBookings(data.booking);
-						setLoading(false);
-					}
-				});
-		}
-	}, []);
+const UserDashboard = () => {
+    const token = useSelector((state) => state.token);
+    const navigate = useNavigate(); 
 
-	return (
-		<div className="min-h-screen">
-			<h1 className="text-3xl text-center">User Dashbord</h1>
-			{loading ? (
-				<h1>Loading...</h1>
-			) : bookings.length === 0 ? (
-				<h1>No bookings found</h1>
-			) : (
-				bookings.map((booking, idx) => {
-					return (
-						<div className="card" key={idx}>
-							<div className="card-itm">
-								<h3>Order Id</h3>
-								<h3>Service Id</h3>
-								<h3>Service Provider Id</h3>
-								<h3>Date</h3>
-								<h3>Booking Location</h3>
-								<h3>Booking Status</h3>
-							</div>
-							<div className="card-itm">
-								<h3>:</h3>
-								<h3>:</h3>
-								<h3>:</h3>
-								<h3>:</h3>
-								<h3>:</h3>
-								<h3>:</h3>
-							</div>
-							<div className="card-itm">
-								<h3>{booking.orderId}</h3>
-								<h3>{booking.serviceId}</h3>
-								<h3>{booking.serviceProviderId}</h3>
-								<h3>{booking.date}</h3>
-								<h3>{booking.bookingLocation}</h3>
-								<h3>{booking.bookingStatus}</h3>
-							</div>
-						</div>
-					);
-				})
-			)}
-			<div className="card">
-				<div className="card-itm">
-					<h3>Order Id</h3>
-					<h3>Service Id</h3>
-					<h3>Service Provider Id</h3>
-					<h3>Date</h3>
-					<h3>Booking Location</h3>
-					<h3>Booking Status</h3>
-				</div>
-				<div className="card-itm">
-					<h3>:</h3>
-					<h3>:</h3>
-					<h3>:</h3>
-					<h3>:</h3>
-					<h3>:</h3>
-					<h3>:</h3>
-				</div>
-				<div className="card-itm">
-					<h3>Test Card</h3>
-					<h3>Test</h3>
-					<h3>Test</h3>
-					<h3>Test</h3>
-					<h3>Test</h3>
-					<h3>Test</h3>
-				</div>
-			</div>
-			<div>
+    useEffect(() => {
+        // If no token is found, redirect to login
+        if (!token) {
+            navigate('/userlogin'); 
+        }
+    }, [token, navigate]); 
+
+    return (
+		<><div>
+			<h1>Welcome to Your Dashboard</h1>
+		</div><div>
+				<Link to="/userbooking"> View Booking </Link>
+			</div><div>
 				<Link to="/wishlist">View Wishlist</Link>
-			</div>
-		</div>
-	);
+			</div></>
+    );
 };
 
-export default UserDashbord;
+export default UserDashboard;
