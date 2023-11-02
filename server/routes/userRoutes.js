526c784c7d362d4ed90c4646cf2fee2fa6d8ca89
Waliullah Ferdous <waliullah.ferdous22@gmail.com>
2023-11-02 18:11:46 +1100

    added booking routes for users

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -7,19 +7,13 @@ const jwtUtils = require("../utils/jwtUtils");
 router.post("/signup", userController.signUp);
 router.post("/login", userController.logIn);
 router.get("/user", jwtUtils.verifyToken, userController.getUser);
-router.get(
-	"/booking",
-	jwtUtils.verifyToken,
-	bookingController.getBookingByUserId
-);
-router.post(
-	"/booking/create",
-	jwtUtils.verifyToken,
-	bookingController.createBooking
-);
 
 router.post("/add-to-wishlist", jwtUtils.verifyToken, userController.addToWishlist);
 router.post("/remove-from-wishlist", jwtUtils.verifyToken, userController.removeFromWishlist);
 router.get("/get-wishlist", jwtUtils.verifyToken, userController.getWishlist);
+
+router.post("/bookings", jwtUtils.verifyToken, bookingController.createBooking); // To create a booking
+router.get("/user-bookings", jwtUtils.verifyToken, bookingController.getUserBookings); // To get bookings for a user
+
 
 module.exports = router;
