c203b244b06de5a96f3e7753914e5a6241fa6b5b
Waliullah Ferdous <waliullah.ferdous22@gmail.com>
2023-11-03 05:09:44 +1100

    added disclaimer to petdetails

--- src/pages/PetDetails.js
+++ src/pages/PetDetails.js
@@ -93,37 +93,40 @@ const PetDetails = () => {
     return (
         <div className="det-container">
             <div className="details-wrapper">
-            <div className="details-left">
-                {/* Displaying pet image or a default image if not available */}
-                <img src={petDetails.photos?.[0]?.medium || 'https://static.vecteezy.com/system/resources/previews/017/047/854/original/cute-cat-illustration-cat-kawaii-chibi-drawing-style-cat-cartoon-vector.jpg'} alt={petDetails.name}
-                    style={{ width: '200px', height: 'auto' }} />
-            </div>
-            <div className="details-right">
-                <h2>{petDetails.name}</h2>  {/* Displaying pet name */}
-                {/* Sanitizing and decoding HTML content for pet description */}
-                <p>Description: {he.decode(sanitizeHtml(petDetails.description, { allowedTags: [] }))} {petDetails.url ? <a href={petDetails.url} target="_blank" rel="noopener noreferrer">
-                    Click Here for More Info
-                </a>
-                    : 'URL not provided'}</p>
-                <p>Status: {petDetails.status}</p>  {/* Displaying pet status */}
-                {/* Displaying contact information if available */}
-                {petDetails.contact && (
-                    <div>
-                        <h3>Contact Information:</h3>
-                        <p>Email: {petDetails.contact.email || 'Not provided'}</p>
-                        <p>Phone: {petDetails.contact.phone || 'Not provided'}</p>
-                        {/* Displaying address information if available */}
-                        {petDetails.contact.address && (
-                            <div>
-                                <p>Address: {petDetails.contact.address.address1 || ''} {petDetails.contact.address.address2 || ''}</p>
-                                <p> {petDetails.contact.address.city}, {petDetails.contact.address.state} {petDetails.contact.address.postcode}, {petDetails.contact.address.country}</p>
-                            </div>
-                        )}
-                    </div>
-                )}
+                <div className="details-left">
+                    {/* Displaying pet image or a default image if not available */}
+                    <img src={petDetails.photos?.[0]?.medium || 'https://static.vecteezy.com/system/resources/previews/017/047/854/original/cute-cat-illustration-cat-kawaii-chibi-drawing-style-cat-cartoon-vector.jpg'} alt={petDetails.name}
+                        style={{ width: '200px', height: 'auto' }} />
+                </div>
+                <div className="details-right">
+                    <h2>{petDetails.name}</h2>  {/* Displaying pet name */}
+                    {/* Sanitizing and decoding HTML content for pet description */}
+                    <p>Description: {he.decode(sanitizeHtml(petDetails.description, { allowedTags: [] }))} {petDetails.url ? <a href={petDetails.url} target="_blank" rel="noopener noreferrer">
+                        Click Here for More Info
+                    </a>
+                        : 'URL not provided'}</p>
+                    <p>Status: {petDetails.status}</p>  {/* Displaying pet status */}
+                    {/* Displaying contact information if available */}
+                    {petDetails.contact && (
+                        <div>
+                            <h3>Contact Information:</h3>
+                            <p>Email: {petDetails.contact.email || 'Not provided'}</p>
+                            <p>Phone: {petDetails.contact.phone || 'Not provided'}</p>
+                            {/* Displaying address information if available */}
+                            {petDetails.contact.address && (
+                                <div>
+                                    <p>Address: {petDetails.contact.address.address1 || ''} {petDetails.contact.address.address2 || ''}</p>
+                                    <p> {petDetails.contact.address.city}, {petDetails.contact.address.state} {petDetails.contact.address.postcode}, {petDetails.contact.address.country}</p>
+                                </div>
+                            )}
+                        </div>
+                    )}
 
-                <button onClick={addToWishlist}>Add to Wishlist</button>
+                    <button onClick={addToWishlist}>Add to Wishlist</button>
+                </div>
             </div>
+            <div className="disclaimer">
+                Disclaimer: This pet information is sourced from a third-party API. We hold no responsibility regarding the adoption process or any other related matters. Our role is merely to display the listing.
             </div>
         </div>
     );
