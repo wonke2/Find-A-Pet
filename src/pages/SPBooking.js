5e1e9255e47624a784f3d6e18f337c2333ee24ce
Waliullah Ferdous <waliullah.ferdous22@gmail.com>
2023-11-04 02:53:02 +1100

    linked css to spbooking js

--- src/pages/SPBooking.js
+++ src/pages/SPBooking.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom'; // Import useNavigate
 import { Link } from 'react-router-dom';
+import "../styles/SPBooking.css";
 
 const SPBooking = () => {
     const [bookings, setBookings] = useState([]);
