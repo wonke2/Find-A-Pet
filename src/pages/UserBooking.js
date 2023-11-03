c10288f84b2251f476813f453048785f878b62b7
Waliullah Ferdous <waliullah.ferdous22@gmail.com>
2023-11-04 02:50:02 +1100

    added classnames for styling

--- src/pages/UserBooking.js
+++ src/pages/UserBooking.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-import '../styles/UserBookings.css';
+import '../styles/UserBooking.css';
 
 
 
@@ -22,34 +22,35 @@ const UserBookings = () => {
                     "Authorization": `Bearer ${token}`,
                 },
             })
-            .then((res) => res.json())
-            .then((data) => {
-                setUserID(data.data._id);
+                .then((res) => res.json())
+                .then((data) => {
+                    setUserID(data.data._id);
 
-                // After getting the userID, fetch the bookings
-                return fetch(`http://localhost:3000/auth/user-bookings/${data.data._id}`, {
-                    headers: {
-                        "Authorization": `Bearer ${token}`,
-                    },
-                });
-            })
-            .then((res) => res.json())
-            .then((data) => setBookings(data))
-            .catch((error) => console.error("Error fetching user bookings:", error));
+                    // After getting the userID, fetch the bookings
+                    return fetch(`http://localhost:3000/auth/user-bookings/${data.data._id}`, {
+                        headers: {
+                            "Authorization": `Bearer ${token}`,
+                        },
+                    });
+                })
+                .then((res) => res.json())
+                .then((data) => setBookings(data))
+                .catch((error) => console.error("Error fetching user bookings:", error));
         }
     }, [token, navigate]);
 
     return (
-        <div>
+        <div className="user-bookings-container">
             <h2>Your Bookings:</h2>
             {bookings.map((booking, index) => (
-                <div key={index}>
-                    <p>Service: {booking.service.serviceName}</p> 
-                    <p>Provider: {booking.serviceProviderName}</p> 
-                    <p>Date: {new Date(booking.bookingDate).toLocaleDateString()}</p>
+                <div key={index} className="booking-card">
+                    <p>Service: <span className="booking-service-name">{booking.service.serviceName}</span></p>
+                    <p>Provider: <span className="booking-provider-name">{booking.serviceProviderName}</span></p>
+                    <p>Date: <span className="booking-date">{new Date(booking.bookingDate).toLocaleDateString()}</span></p>
                 </div>
             ))}
         </div>
+
     );
 };
 
