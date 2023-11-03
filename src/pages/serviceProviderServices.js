b0526cceb0c8ab120e52ea60ebf8c20532759984
wonke1 <100748961+wonke1@users.noreply.github.com>
2023-11-03 19:11:36 +1100

    finished services page

---
+++ src/pages/serviceProviderServices.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+
+const ServiceProviderServices = () => {
+  const [SPToken, setSPToken] = useState(localStorage.getItem("SPToken"));
+  const [services, setServices] = useState([]);
+
+  useEffect(() => {
+    async function fetchServices() {
+      try {
+        const response = await fetch("/SPAuth/services", {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${SPToken}`,
+          },
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          setServices(data);
+        } else {
+          console.error("Failed to fetch services");
+        }
+      } catch (error) {
+        console.error("Error fetching services:", error);
+      }
+    }
+
+    fetchServices();
+  }, [SPToken]);
+
+  return (
+    <div>
+      <h1>Your Services</h1>
+      <ul>
+        {services.map((service) => (
+          <li key={service._id}>
+            <strong>Service Name:</strong> {service.serviceName}<br />
+            <strong>Service Description:</strong> {service.serviceDescription}<br />
+            <strong>Service Location:</strong> {service.serviceLocation}<br />
+          </li>
+        ))}
+          </ul>
+        <Link to="/spdashboard/sp-services/addservice">Add a Service</Link>
+    </div>
+  );
+};
+
+export default ServiceProviderServices;
