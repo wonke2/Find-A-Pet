4c7122e194fd8332699c77bc589b25b6b6efd847
Waliullah Ferdous <waliullah.ferdous22@gmail.com>
2023-11-04 03:28:18 +1100

    removed logout from dashboard

--- src/pages/SPDashboard.js
+++ src/pages/SPDashboard.js
@@ -4,13 +4,12 @@ import "../styles/SPDash.css";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from 'react-redux';
-import { setLogout } from '../state/authSlice';
 
 const SPDashboard = () => {
     const navigate = useNavigate();
     const dispatch = useDispatch();
     const [SPToken, setSPToken] = useState(localStorage.getItem("SPToken"));
-    const [serviceProvider, setServiceProvider] = useState(null);
+ 
 
     useEffect(() => {
         if (!SPToken) {
@@ -18,17 +17,12 @@ const SPDashboard = () => {
         }
     }, [SPToken, navigate]);
 
-    const handleLogout = () => {
-        dispatch(setLogout());
-        navigate('/splogin');
-    }
 
     return (
         <div>
             <h1>Service Provider Dashboard</h1>
             <Link to="/spdashboard/sp-services">View Services</Link>
-            <Link to="/spdashboard/sp-bookings">View Bookings</Link> {/* Link to the SPBookings component */}
-            <button onClick={handleLogout}>Logout</button>
+            <Link to="/spdashboard/sp-bookings">View Bookings</Link>
         </div>
     );
 }
