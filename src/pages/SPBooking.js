4022d037b74797f4f8e491af7754574cf190a945
Waliullah Ferdous <waliullah.ferdous22@gmail.com>
2023-11-04 03:04:15 +1100

    added classnames for spbooking styles

--- src/pages/SPBooking.js
+++ src/pages/SPBooking.js
@@ -23,39 +23,39 @@ const SPBooking = () => {
                 "Authorization": `Bearer ${SPToken}`,
             },
         })
-        .then((res) => {
-            if (!res.ok) {
-                throw new Error(`Server responded with status: ${res.status} for SP user request`);
-            }
-            return res.json();
-        })
-        .then((data) => {
-            console.log('SPuser data:', data); 
-            if (data && data.data && data.data.id) {
-                // After getting the serviceProviderID, fetch the bookings
-                return fetch(`/SPAuth/bookings/service-provider/${data.data.id}`, {
-                    headers: {
-                        "Authorization": `Bearer ${SPToken}`,
-                    },
-                });
-            } else {
-                throw new Error('Failed to retrieve service provider ID.');
-            }
-        })
-        .then((res) => {
-            if (!res.ok) {
-                throw new Error(`Server responded with status: ${res.status} for bookings request`);
-            }
-            return res.json();
-        })
-        .then((data) => {
-            setIsLoading(false);
-            setBookings(data);
-        })
-        .catch((error) => {
-            setIsLoading(false);
-            console.error("Error fetching SP bookings:", error);
-        });
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error(`Server responded with status: ${res.status} for SP user request`);
+                }
+                return res.json();
+            })
+            .then((data) => {
+                console.log('SPuser data:', data);
+                if (data && data.data && data.data.id) {
+                    // After getting the serviceProviderID, fetch the bookings
+                    return fetch(`/SPAuth/bookings/service-provider/${data.data.id}`, {
+                        headers: {
+                            "Authorization": `Bearer ${SPToken}`,
+                        },
+                    });
+                } else {
+                    throw new Error('Failed to retrieve service provider ID.');
+                }
+            })
+            .then((res) => {
+                if (!res.ok) {
+                    throw new Error(`Server responded with status: ${res.status} for bookings request`);
+                }
+                return res.json();
+            })
+            .then((data) => {
+                setIsLoading(false);
+                setBookings(data);
+            })
+            .catch((error) => {
+                setIsLoading(false);
+                console.error("Error fetching SP bookings:", error);
+            });
     }, [navigate]); // Add navigate to the dependency array
 
     const removeBooking = (bookingId) => {
@@ -67,41 +67,41 @@ const SPBooking = () => {
                 "Content-Type": "application/json",
             },
         })
-        .then(res => res.json())
-        .then(response => {
-            if (response.status === 'success') {
-                // Remove the deleted booking from the state to update the UI
-                setBookings(bookings.filter(booking => booking._id !== bookingId));
-                console.log('Booking removed successfully');
-            } else {
-                console.error('Failed to remove booking:', response.message);
-            }
-        })
-        .catch(error => console.error('Error deleting booking:', error));
+            .then(res => res.json())
+            .then(response => {
+                if (response.status === 'success') {
+                    // Remove the deleted booking from the state to update the UI
+                    setBookings(bookings.filter(booking => booking._id !== bookingId));
+                    console.log('Booking removed successfully');
+                } else {
+                    console.error('Failed to remove booking:', response.message);
+                }
+            })
+            .catch(error => console.error('Error deleting booking:', error));
     };
 
     // Render the bookings
     return (
-        <div>
-            <Link to="/spdashboard">Back to Dashboard</Link>
+        <div className="spbooking-container">
+            <Link to="/spdashboard" className="dashboard-link">Back to Dashboard</Link>
             <h1>Your Bookings</h1>
             {isLoading ? (
-                <p>Loading bookings...</p>
+                <p className="loading-message">Loading bookings...</p>
             ) : bookings.length > 0 ? (
                 bookings.map((booking) => (
-                    <div key={booking._id}>
+                    <div key={booking._id} className="booking-details">
                         <ul>
                             <li>Booking ID: {booking._id}</li>
                             <li>Service Name: {booking.service && booking.service.serviceName}</li>
-                            <li>User Name: {booking.user && booking.user.username}</li>
-                            <li>User Email: {booking.user && booking.user.email}</li>
+                            <li>User Name: {booking.userID && booking.userID.username}</li>
+                            <li>User Email: {booking.userID && booking.userID.email}</li>
                             <li>Booking Date: {booking.bookingDate && new Date(booking.bookingDate).toLocaleDateString()}</li>
                         </ul>
-                        <button onClick={() => removeBooking(booking._id)}>Remove Booking</button>
+                        <button onClick={() => removeBooking(booking._id)} className="remove-booking-btn">Remove Booking</button>
                     </div>
                 ))
             ) : (
-                <p>No bookings found.</p>
+                <p className="no-bookings-message">No bookings found.</p>
             )}
         </div>
     );
