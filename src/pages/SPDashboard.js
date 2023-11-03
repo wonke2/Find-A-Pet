da5a87ee56729e0d1240fe3d74da045d0fa522a0
Waliullah Ferdous <waliullah.ferdous22@gmail.com>
2023-11-03 21:11:48 +1100

    removed fetching booking from spdashboard

--- src/pages/SPDashboard.js
+++ src/pages/SPDashboard.js
@@ -1,46 +1,28 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "../styles/SPDash.css";
-import { useState, useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from 'react-redux';
 import { setLogout } from '../state/authSlice';
 
 const SPDashboard = () => {
     const navigate = useNavigate();
-    const [bookings, setBookings] = useState([]);
-    const [SPToken, setSPToken] = useState(localStorage.getItem("SPToken"));
-    const [loading, setLoading] = useState(true);
     const dispatch = useDispatch();
+    const [SPToken, setSPToken] = useState(localStorage.getItem("SPToken"));
     const [serviceProvider, setServiceProvider] = useState(null);
 
     useEffect(() => {
         if (!SPToken) {
             navigate("/splogin");
         } else {
-            const fetchBookings = async () => {
-                try {
-                    const response = await fetch(`/bookings`, {
-                        method: 'GET',
-                        headers: {
-                            'Authorization': `Bearer ${SPToken}`
-                        }
-                    });
-                    const data = await response.json();
-                    setBookings(data);
-                    setLoading(false);
-                } catch (error) {
-                    console.error("Error fetching bookings:", error);
-                    setLoading(false);
-                }
-            }
-
+            // Fetch service provider details only
             const fetchServiceProviderDetails = async () => {
                 try {
                     const response = await fetch(`/SPauth/SPuser/`, {
                         method: "GET",
                         headers: {
-                        Authorization: `Bearer ${SPToken}`,
+                            'Authorization': `Bearer ${SPToken}`,
                         },
                     });
                     const contentType = response.headers.get("content-type");
@@ -56,12 +38,11 @@ const SPDashboard = () => {
                         console.error("Unexpected response content type");
                     }
                 } catch (error) {
-                console.error("Error retrieving service provider details:", error);
+                    console.error("Error retrieving service provider details:", error);
                 }
             };
 
             fetchServiceProviderDetails();
-            fetchBookings();
         }
     }, [SPToken, navigate]);
 
@@ -74,37 +55,22 @@ const SPDashboard = () => {
         <div>
             <h1>Service Provider Dashboard</h1>
             <Link to="/spdashboard/sp-services">View Services</Link>
-                <div>
-                    <h1>Service Provider Details</h1>
-                    {serviceProvider ? (
-                        <div>
+            <Link to="/spdashboard/sp-bookings">View Bookings</Link> {/* Link to the SPBookings component */}
+            <div>
+                <h2>Service Provider Details</h2>
+                {serviceProvider ? (
+                    <div>
                         <p>Service Provider Name: {serviceProvider.serviceProviderName}</p>
                         <p>Organization Name: {serviceProvider.orgName}</p>
                         <p>Service Provider Address: {serviceProvider.serviceProviderAddress}</p>
                         <p>Service Provider Email: {serviceProvider.serviceProviderEmail}</p>
                         <p>Service Provider Phone: {serviceProvider.serviceProviderPhone}</p>
                         {/* Display other details as needed */}
-                        </div>
-                    ) : (
-                        <p>Loading...</p>
-                    )}
-                </div>
-            {loading ? <p>Loading...</p> : (
-            <div>
-                {bookings.map(booking => (
-                    <div key={booking._id}>
-                        <ul>
-                            <li>Booking ID: {booking._id}</li>
-                            <li>Service ID: {booking.serviceID}</li>
-                            <li>User ID: {booking.userID}</li>
-                            <li>Booking Date: {booking.Date}</li>
-                            <li>Booking Location: {booking.location}</li>
-                            <li>Booking Status: {booking.status}</li>
-                        </ul>
                     </div>
-                ))}
+                ) : (
+                    <p>Loading service provider details...</p>
+                )}
             </div>
-            )}
             <button onClick={handleLogout}>Logout</button>
         </div>
     );
