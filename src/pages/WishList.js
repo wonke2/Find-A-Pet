74002e36ed41b27d6f918ad63cd94daad0485202
Waliullah Ferdous <waliullah.ferdous22@gmail.com>
2023-11-03 16:30:38 +1100

    linked wishlist css to js

--- src/pages/WishList.js
+++ src/pages/WishList.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
+import '../styles/WishList.css';
 
 const Wishlist = () => {
     const [wishlist, setWishlist] = useState([]);
