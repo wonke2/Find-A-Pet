27df3626090be51ddd26e7ed8d4dc67cb0cc37ec
Waliullah Ferdous <waliullah.ferdous22@gmail.com>
2023-11-04 02:26:04 +1100

    authorization check on spbooking

--- src/pages/SPBooking.js
+++ src/pages/SPBooking.js
@@ -1,57 +1,60 @@
 import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom'; // Import useNavigate
 
 const SPBooking = () => {
     const [bookings, setBookings] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const navigate = useNavigate(); // Instantiate useNavigate
 
     useEffect(() => {
         const SPToken = localStorage.getItem('SPToken');
 
-        if (SPToken) {
-            // Fetch the service provider's ID first
-            fetch("/SPAuth/SPuser/", {
-                headers: {
-                    "Authorization": `Bearer ${SPToken}`,
-                },
-            })
-            .then((res) => {
-                if (!res.ok) {
-                    throw new Error(`Server responded with status: ${res.status} for SP user request`);
-                }
-                return res.json();
-            })
-            .then((data) => {
-                console.log('SPuser data:', data); 
-                if (data && data.data && data.data.id) {
-                    // After getting the serviceProviderID, fetch the bookings
-                    return fetch(`/SPAuth/bookings/service-provider/${data.data.id}`, {
-                        headers: {
-                            "Authorization": `Bearer ${SPToken}`,
-                        },
-                    });
-                } else {
-                    throw new Error('Failed to retrieve service provider ID.');
-                }
-            })
-            .then((res) => {
-                if (!res.ok) {
-                    throw new Error(`Server responded with status: ${res.status} for bookings request`);
-                }
-                return res.json();
-            })
-            .then((data) => {
-                setIsLoading(false);
-                setBookings(data);
-            })
-            .catch((error) => {
-                setIsLoading(false);
-                console.error("Error fetching SP bookings:", error);
-            });
-        } else {
-            setIsLoading(false);
-            console.error("SPToken not found. Please log in.");
+        // Redirect to the login page if there's no token
+        if (!SPToken) {
+            navigate('/SPlogin'); // Navigate to login page
+            return; // Return early to avoid making unnecessary fetch calls
         }
-    }, []);
+
+        // Fetch the service provider's ID first
+        fetch("/SPAuth/SPuser/", {
+            headers: {
+                "Authorization": `Bearer ${SPToken}`,
+            },
+        })
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error(`Server responded with status: ${res.status} for SP user request`);
+            }
+            return res.json();
+        })
+        .then((data) => {
+            console.log('SPuser data:', data); 
+            if (data && data.data && data.data.id) {
+                // After getting the serviceProviderID, fetch the bookings
+                return fetch(`/SPAuth/bookings/service-provider/${data.data.id}`, {
+                    headers: {
+                        "Authorization": `Bearer ${SPToken}`,
+                    },
+                });
+            } else {
+                throw new Error('Failed to retrieve service provider ID.');
+            }
+        })
+        .then((res) => {
+            if (!res.ok) {
+                throw new Error(`Server responded with status: ${res.status} for bookings request`);
+            }
+            return res.json();
+        })
+        .then((data) => {
+            setIsLoading(false);
+            setBookings(data);
+        })
+        .catch((error) => {
+            setIsLoading(false);
+            console.error("Error fetching SP bookings:", error);
+        });
+    }, [navigate]); // Add navigate to the dependency array
 
     const removeBooking = (bookingId) => {
         const SPToken = localStorage.getItem('SPToken');
@@ -74,7 +77,6 @@ const SPBooking = () => {
         })
         .catch(error => console.error('Error deleting booking:', error));
     };
-    
 
     // Render the bookings
     return (
@@ -87,10 +89,9 @@ const SPBooking = () => {
                     <div key={booking._id}>
                         <ul>
                             <li>Booking ID: {booking._id}</li>
-                            {/* Make sure the rest of the properties are being correctly accessed */}
                             <li>Service Name: {booking.service && booking.service.serviceName}</li>
-                            <li>User Name: {booking.userID && booking.userID.username}</li>
-                            <li>User Email: {booking.userID && booking.userID.email}</li>
+                            <li>User Name: {booking.user && booking.user.username}</li>
+                            <li>User Email: {booking.user && booking.user.email}</li>
                             <li>Booking Date: {booking.bookingDate && new Date(booking.bookingDate).toLocaleDateString()}</li>
                         </ul>
                         <button onClick={() => removeBooking(booking._id)}>Remove Booking</button>
@@ -99,7 +100,6 @@ const SPBooking = () => {
             ) : (
                 <p>No bookings found.</p>
             )}
-            
         </div>
     );
 };
