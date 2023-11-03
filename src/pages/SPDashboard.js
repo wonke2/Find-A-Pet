c2fa20cc5ff3add35e9e58dd6d00aa72c94303c9
Waliullah Ferdous <waliullah.ferdous22@gmail.com>
2023-11-04 03:39:34 +1100

    added spdash icons and classnames

--- src/pages/SPDashboard.js
+++ src/pages/SPDashboard.js
@@ -3,13 +3,14 @@ import { Link } from "react-router-dom";
 import "../styles/SPDash.css";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { useDispatch } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCalendarCheck, faList } from '@fortawesome/free-solid-svg-icons';
+
 
 const SPDashboard = () => {
     const navigate = useNavigate();
-    const dispatch = useDispatch();
     const [SPToken, setSPToken] = useState(localStorage.getItem("SPToken"));
- 
+
 
     useEffect(() => {
         if (!SPToken) {
@@ -19,10 +20,18 @@ const SPDashboard = () => {
 
 
     return (
-        <div>
-            <h1>Service Provider Dashboard</h1>
-            <Link to="/spdashboard/sp-services">View Services</Link>
-            <Link to="/spdashboard/sp-bookings">View Bookings</Link>
+        <div className="sp-dashboard-container">
+            <h1 className="user-dashboard-title">Service Provider Dashboard</h1>
+            <div className="dashboard-tile-container">
+                <Link to="/spdashboard/sp-services" className="dashboard-tile">
+                    <FontAwesomeIcon icon={faList} size="2x" className="dashboard-tile-icon" />
+                    <span>View Services</span>
+                </Link>
+                <Link to="/spdashboard/sp-bookings" className="dashboard-tile">
+                    <FontAwesomeIcon icon={faCalendarCheck} size="2x" className="dashboard-tile-icon" />
+                    <span>View Bookings</span>
+                </Link>
+            </div>
         </div>
     );
 }
