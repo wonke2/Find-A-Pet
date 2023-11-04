39c6552417bd30b6c49ce3d4431c729e45c6dd62
Waliullah Ferdous <waliullah.ferdous22@gmail.com>
2023-11-05 03:47:29 +1100

    added comments to wishlist file

--- src/pages/WishList.js
+++ src/pages/WishList.js
@@ -1,24 +1,27 @@
 import React, { useState, useEffect } from "react";
-import { useNavigate, Link } from "react-router-dom"; // Import useNavigate from react-router-dom
-import { useSelector } from "react-redux";
-import '../styles/WishList.css';
-import sanitizeHtml from 'sanitize-html';  // For sanitizing the HTML content
-import he from 'he';  // For decoding HTML entities
+import { useNavigate, Link } from "react-router-dom"; // For routing and navigation
+import { useSelector } from "react-redux"; // To access Redux store state
+import '../styles/WishList.css'; // Wishlist-specific styles
+import sanitizeHtml from 'sanitize-html'; // To prevent XSS attacks by sanitizing user input
+import he from 'he'; // To decode HTML entities
 
+// Wishlist component
 const Wishlist = () => {
-    const [wishlist, setWishlist] = useState([]);
-    const navigate = useNavigate(); // Create a navigate function
-    const token = useSelector((state) => state.token);
+    const [wishlist, setWishlist] = useState([]);// Local state to store wishlist items
+    const navigate = useNavigate(); // Hook for navigating programmatically
+    const token = useSelector((state) => state.token); // Retrieve the authentication token from the Redux store
 
+    // Fetch wishlist items on component mount and when the token changes
     useEffect(() => {
         const fetchWishlist = async () => {
             if (!token) {
-                // If there's no token, redirect to the login page
+                // Redirect to login if there's no token
                 navigate('/userlogin');
                 return;
             }
 
             try {
+                // Attempt to fetch wishlist data from the API
                 const response = await fetch("http://localhost:3000/auth/get-wishlist", {
                     headers: {
                         "Authorization": `Bearer ${token}`
@@ -26,19 +29,22 @@ const Wishlist = () => {
                 });
 
                 if (!response.ok) {
+                    // Handle non-2xx HTTP responses
                     if (response.status === 401) {
-                        // If the response status is 401, it means the token is no longer valid
+                        // Token expired or invalid - redirect to login
                         navigate('/userlogin');
                         return;
                     }
                     throw new Error('Network response was not ok.');
                 }
 
+                // Process the response data
                 const data = await response.json();
                 if (data.status === "success") {
+                    // Update local state with fetched wishlist
                     setWishlist(data.wishlist);
                 } else {
-                    // If the token is not valid (e.g., expired), navigate to the login page
+                    // Handle other issues, like invalid token
                     if (data.message === 'Token is not valid') {
                         navigate('/userlogin');
                     } else {
@@ -46,6 +52,7 @@ const Wishlist = () => {
                     }
                 }
             } catch (error) {
+                // Catch fetch errors
                 console.error('There has been a problem with your fetch operation:', error);
             }
         };
@@ -53,7 +60,9 @@ const Wishlist = () => {
         fetchWishlist();
     }, [token, navigate]); // Added token as a dependency since it's being used inside useEffect
 
+    // Function to remove an item from the wishlist
     const removeFromWishlist = async (petID) => {
+        // Send the request to remove the item from the wishlist
         const response = await fetch("http://localhost:3000/auth/remove-from-wishlist", {
             method: "POST",
             headers: {
@@ -65,12 +74,15 @@ const Wishlist = () => {
 
         const data = await response.json();
         if (data.status === "fail") {
+            // Alert the user if there was a problem
             alert(data.message);
         } else {
+            // Update the wishlist state to remove the item
             setWishlist(prevWishlist => prevWishlist.filter(pet => pet.petID !== petID));
         }
     };
 
+    // Render the Wishlist component
     return (
         <div className="wishlist-container">
             <Link to="/userdashboard" className="userdashboard-link">Back to Dashboard</Link>
@@ -78,8 +90,8 @@ const Wishlist = () => {
             <ul>
                 {wishlist.map(pet => (
                     <li key={pet.petID}>
-                        <img src={pet.petImage || 'https://static.vecteezy.com/system/resources/previews/017/047/854/original/cute-cat-illustration-cat-kawaii-chibi-drawing-style-cat-cartoon-vector.jpg'} 
-                        alt={he.decode(sanitizeHtml(pet.petName, { allowedTags: [] }))}
+                        <img src={pet.petImage || 'https://static.vecteezy.com/system/resources/previews/017/047/854/original/cute-cat-illustration-cat-kawaii-chibi-drawing-style-cat-cartoon-vector.jpg'}
+                            alt={he.decode(sanitizeHtml(pet.petName, { allowedTags: [] }))}
                             style={{ width: '200px', height: 'auto' }} />
                         <Link to={`/pet/${pet.petID}`} className="wishlistname-link">{he.decode(sanitizeHtml(pet.petName, { allowedTags: [] }))}</Link>
                         <button onClick={() => removeFromWishlist(pet.petID)}>Remove</button>
