83cea5798ac48b955623f6c1d16056f199132197
wonke1 <100748961+wonke1@users.noreply.github.com>
2023-11-05 01:38:03 +1100

    Fix import statement for SPCDashboard.css.

--- src/pages/SPDashboard.js
+++ src/pages/SPDashboard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "../styles/SPDash.css";
+import "../styles/SPDashboard.css";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
