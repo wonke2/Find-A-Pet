a141d7fc39540af4e87d268137933e1d8c29f3a8
wonke1 <100748961+wonke1@users.noreply.github.com>
2023-11-05 11:52:46 +1100

    Add CSS file for detailed service listing page

--- src/pages/detailedServiceListing.js
+++ src/pages/detailedServiceListing.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useParams, useNavigate } from 'react-router-dom';
 import { useSelector } from "react-redux";
+import '../styles/DetailedServiceListing.css';
 
 const DetailedServiceListing = () => {
     const { serviceID } = useParams();
