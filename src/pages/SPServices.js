202b1c73a4108e42521ac2450bbd90ea4fa69ec0
wonke1 <100748961+wonke1@users.noreply.github.com>
2023-11-05 13:46:04 +1100

    Add SPServices.css file and import it in SPServices.js

--- src/pages/SPServices.js
+++ src/pages/SPServices.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { confirmAlert } from "react-confirm-alert";
+import "../styles/SPServices.css";
 
 const SPServices = () => {
   const [SPToken, setSPToken] = useState(localStorage.getItem("SPToken"));
