00495270c6b455e893cf1d4d3b5175f46df042bf
wonke1 <100748961+wonke1@users.noreply.github.com>
2023-11-05 18:59:29 +1100

    fixed accidental removal of fields

--- src/pages/SPSignup.js
+++ src/pages/SPSignup.js
@@ -84,8 +84,114 @@ const SPSignup = () => {
                         setUsername(e.target.value);
                     }}
                 />
-                {/* More input fields and validation checks */}
-                { /* ... */ }
+                <label htmlFor="organizationName">Organization Name <i>*</i></label>
+                <input
+                    type="text"
+                    id="organizationName"
+                    required
+                    onChange={(e) => {
+                        setOrgName(e.target.value);
+                    }}
+                />
+
+                <label htmlFor="password">Password <i>*</i></label>
+                <input
+                    type="password"
+                    id="password"
+                    required
+                    onChange={(e) => {
+                        const newPassword = e.target.value;
+                        setPassword(newPassword)
+                        setPasswordsMatch(newPassword === confirmPassword);
+                        const isWeak = !/(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/.test(newPassword);
+                        setIsPasswordWeak(isWeak);
+                        if (isWeak) {
+                            setPasswordErrorMessage("Password is too weak.");
+                        } else {
+                            setPasswordErrorMessage("");
+                        }
+                    }}
+                />
+                <br/>
+                <label htmlFor="confirmPassword">Confirm Password <i>*</i></label>
+                <input
+                    type="password"
+                    id="confirmPassword"
+                    required
+                    onChange={(e) => {
+                        setConfirmPassword(e.target.value);
+                        setPasswordsMatch(e.target.value === password);
+                        setIsConfirmPassword(e.target.value === password);
+                        if (e.target.value !== password) {
+                            setPasswordErrorMessage("Passwords do not match.");
+                        }
+                        else {
+                            setPasswordErrorMessage("");
+                        }
+                    }}
+                />
+                <label htmlFor="email">Email <i>*</i></label>
+                <input
+                    type="text"
+                    id="email"
+                    required
+                    onChange={(e) => {
+                        setEmail(e.target.value)
+                                const isEmailPatternValid = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(e.target.value);
+                        setIsEmailValid(isEmailPatternValid);
+                        if (!isEmailPatternValid) {
+                            setEmailErrorMessage("Invalid email address.");
+                        } else {
+                            setEmailErrorMessage("");
+                        }
+                    }}
+                />
+                <label htmlFor="phoneNo">Phone Number <i>*</i></label>
+                <input
+                    type="tel"
+                    required
+                    id="phoneNo"
+                    pattern ="/^\d{10}$/"
+                    onChange={(e) => {
+                        setPhoneNo(e.target.value)
+                        const isPhonePatternValid = /^\d{10}$/.test(e.target.value);
+                        setIsPhoneValid(isPhonePatternValid);
+                        if (!isPhonePatternValid) {
+                            setPhoneErrorMessage("Invalid Phone Number.");
+                        } else {
+                            setPhoneErrorMessage("");
+                        }
+                    }}
+                />
+                <label htmlFor="location">Location</label>
+                <textarea
+                    name=""
+                    id="location"
+                    rows="3"
+                    onChange={(e) => {
+                        setLocation(e.target.value)
+                    }}
+                />
+                {showIncompleteFieldsBanner && (
+                    <p className={styles.err}>
+                        Please fill out all required (*) fields.
+                    </p>
+                )}
+                {isPasswordWeak || !isConfirmPassword && (
+                        <p className={styles.err}>
+                            {passwordErrorMessage}
+                        </p>
+                )}
+                {!isPhoneValid && (
+                    <p className={styles.err}>
+                        {phoneErrorMessage}
+                    </p>
+                )}
+                {!isEmailValid && (
+                    <p className={styles.err}>
+                        {emailErrorMessage}
+                    </p>
+                )}
                 <div onClick={signup} disabled={!passwordsMatch || isPasswordWeak || password === "" || !isPhoneValid || !isEmailValid} className={styles.button}>Signup</div>
                 <div className={styles.social}>
                     <Link to="/SPlogin"> Login </Link>
