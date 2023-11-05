538a4a0cb07221df68d4f440bbccfe6fc7a39ee6
wonke1 <100748961+wonke1@users.noreply.github.com>
2023-11-05 17:54:36 +1100

    comments added

--- src/pages/UserLogin.js
+++ src/pages/UserLogin.js
@@ -5,71 +5,83 @@ import { useDispatch, useSelector } from "react-redux";
 import styles from "../styles/UserLogin.module.css";
 
 const UserLogin = () => {
-	const [username, setUsername] = useState("");
-	const [password, setPassword] = useState("");
-	const SPToken = useSelector((state) => state.SPToken);
-	const token = useSelector((state) => state.token)
-	const navigate = useNavigate();
-	const dispatch = useDispatch();
+    // State to manage the entered username and password
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    // Get the service provider token and user token from the Redux store
+    const SPToken = useSelector((state) => state.SPToken);
+    const token = useSelector((state) => state.token);
+    // Create a navigate function to handle routing
+    const navigate = useNavigate();
+    // Get the dispatch function from Redux
+    const dispatch = useDispatch();
 
-	useEffect(() => {
-		if (SPToken) {
-			navigate("/serviceprovider");
-		}
-		if (token) {
-			navigate("/user");
-		}
-	}, [SPToken, token, navigate])
-	
-	const login = async () => {
-		const res = await fetch("http://localhost:3000/auth/login", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-			},
-			body: JSON.stringify({ username, password }),
-		});
-		const data = await res.json();
-		if (data.status === "fail") {
-			alert(data.message);
-		} else {
-			dispatch(setLogin({ token: data.token }));
-			navigate("/user");
-		}
-	};
-	return (
-		<div className={styles.center}>
-			<form>
-				<h3>Login Here</h3>
+    useEffect(() => {
+        // If a service provider token exists, redirect to the service provider dashboard
+        if (SPToken) {
+            navigate("/serviceprovider");
+        }
+        // If a user token exists, redirect to the user dashboard
+        if (token) {
+            navigate("/user");
+        }
+    }, [SPToken, token, navigate]);
 
-				<label>Username</label>
-				<input
-					type="text"
-					placeholder="username"
-					id="username"
-					onChange={(event) => {
-						setUsername(event.target.value);
-					}}
-				/>
+    // Function to handle user login
+    const login = async () => {
+        const res = await fetch("http://localhost:3000/auth/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username, password }),
+        });
+        const data = await res.json();
+        if (data.status === "fail") {
+            alert(data.message);
+        } else {
+            // Dispatch a Redux action to set the user token
+            dispatch(setLogin({ token: data.token }));
+            // Redirect to the user dashboard
+            navigate("/user");
+        }
+    };
 
-				<label>Password</label>
-				<input
-					type="password"
-					placeholder="Password"
-					id="password"
-					onChange={(event) => {
-						setPassword(event.target.value);
-					}}
-				/>
+    return (
+        <div className={styles.center}>
+            <form>
+                <h3>Login Here</h3>
 
-				<div className={styles.button} onClick={login}>Log In</div>
-				<div className={styles.social}>
-					<Link to="/usersignup">Signup</Link>
-					<Link to="/splogin">Service Provider</Link>
-				</div>
-			</form>
-		</div>
-	);
+                <label>Username</label>
+                <input
+                    type="text"
+                    placeholder="username"
+                    id="username"
+                    onChange={(event) => {
+                        setUsername(event.target.value);
+                    }}
+                />
+
+                <label>Password</label>
+                <input
+                    type="password"
+                    placeholder="Password"
+                    id="password"
+                    onChange={(event) => {
+                        setPassword(event.target.value);
+                    }}
+                />
+
+                {/* Button to trigger the login function */}
+                <div className={styles.button} onClick={login}>Log In</div>
+                <div className={styles.social}>
+                    {/* Links to the user signup and service provider login pages */}
+                    <Link to="/usersignup">Signup</Link>
+                    <Link to="/splogin">Service Provider</Link>
+                </div>
+            </form>
+        </div>
+    );
 };
 
 export default UserLogin;
